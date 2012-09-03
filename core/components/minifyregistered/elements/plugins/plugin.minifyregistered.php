9ea1b36b78a04f6f18715608f96519f75dfd7ce2
Thomas Jakobi <thomas.jakobi@partout.info>
2012-09-03 23:42:00 +0200

    Initial Release

--- core/components/minifyregistered/elements/plugins/plugin.minifyregistered.php
+++ core/components/minifyregistered/elements/plugins/plugin.minifyregistered.php
@@ -1,10 +1,29 @@
 <?php
 /**
  * minifyRegistered
+ *
+ * Copyright 2011 by Thomas Jakobi <thomas.jakobi@partout.info>
+ *
+ * minifyRegistered is free software; you can redistribute it and/or modify it 
+ * under the terms of the GNU General Public License as published by the Free 
+ * Software Foundation; either version 2 of the License, or (at your option) any 
+ * later version.
+ *
+ * minifyRegistered is distributed in the hope that it will be useful, but 
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * Rowboat; if not, write to the Free Software Foundation, Inc., 59 Temple
+ * Place, Suite 330, Boston, MA 02111-1307 USA
+ *
+ * @package minifyregistered
+ * @subpackage plugin
  * 
- * @author      Jako (thomas.jakobi@partout.info)
- * @copyright   Copyright 2011, Jako
- * @version     0.2 revo
+ * @author      Thomas Jakobi (thomas.jakobi@partout.info)
+ * @copyright   Copyright 2011, Thomas Jakobi
+ * @version     0.2r
  *
  * @internal    events: OnWebPagePrerender
  * @internal    parameter: 
